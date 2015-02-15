8f650e3726e27ac6ee7d3a15b9aa1da2fccbbaa9
toukato <toukato0313@gmail.com>
2015-02-15 22:24:50 +0900

    記録方法の変更
    １００MBでファイルを生成するように処理を変更。

--- BurpExtender.java
+++ BurpExtender.java
@@ -1,6 +1,7 @@
 package burp;
 
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
@@ -14,12 +15,27 @@ public class BurpExtender implements IBurpExtender, IHttpListener
     	callbacks.registerHttpListener(this);
     }
     
+    int number = 1;
+    
     public void processHttpMessage(int toolFlag, boolean messageIsRequest, IHttpRequestResponse messageInfo)
     {
     	Date date = new Date();
-    	SimpleDateFormat simpledateformat = new SimpleDateFormat("yyyyMMddHH.");
+    	SimpleDateFormat simpledateformat = new SimpleDateFormat("yyyyMMdd");
     	String dateString = simpledateformat.format(date);
-    	String autolog = dateString + "log";
+    	
+    	String logFiles = "";
+    	logFiles = dateString + "_" + String.format("%02d", number) + ".log";
+		File file = new File(logFiles);
+		
+		if(file.exists())
+		{
+			if(file.length() >= 104857600)
+			{
+				number = number + 1;
+				logFiles = dateString + "_" + String.format("%02d", number) + ".log";
+			}
+		}
+		
     	String request = "";
     	String response = "";
     	SimpleDateFormat simpledateformat1 = new SimpleDateFormat("HH:mm:ss");
@@ -30,7 +46,7 @@ public class BurpExtender implements IBurpExtender, IHttpListener
     		if(messageInfo.getResponse() != null)
     		{
     		
-    			FileWriter filewriter = new FileWriter(autolog, true);
+    			FileWriter filewriter = new FileWriter(logFiles, true);
     			BufferedWriter bufferedwriter = new BufferedWriter(filewriter);
     			request = new String(messageInfo.getRequest());
     			response = new String(messageInfo.getResponse());
